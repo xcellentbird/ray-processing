e93b41904d1abad3cb23a7653ffc8f73a4962892
mike <xcellentbird@gmail.com>
2023-11-23 23:56:25 +0900

    데이터 경로 수정, 처리량 수정

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import ray
 from attr_tagging.tagger import AttrTagProcess
 from wat_extractor.extractor import WatExtractProcess, download_wat_paths_file
 
-DATA_ROOT = Path('mnt', 'az-files', 'data')
+DATA_ROOT = Path('/mnt', 'az-files', 'data')
 WAT_PATHS_PATH = Path(DATA_ROOT, 'wat.paths')
 
 if __name__ == '__main__':
@@ -15,7 +15,7 @@ if __name__ == '__main__':
     download_wat_paths_file(WAT_PATHS_PATH)
 
     extracted_path = Path(DATA_ROOT, 'extracted.parquet')
-    WatExtractProcess(WAT_PATHS_PATH).execute(extracted_path)
+    WatExtractProcess(WAT_PATHS_PATH).execute(extracted_path, num_wat_paths=256)
 
     tagged_path = Path(DATA_ROOT, 'tagged.parquet')
     AttrTagProcess(extracted_path).execute(tagged_path)
